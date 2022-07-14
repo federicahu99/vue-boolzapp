bdcd582b3ff81833ba02c3c05ba1b8f6aba77a05
feddericahu99 <federica.hu339@gmail.com>
2022-07-14 16:44:40 +0200

    added avatar and names from js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log('ok', Vue)
+
+const root = new Vue ({
+    el: "#root",
+    data : {
+          img: '',
+          contacts: [
+            {
+              name: 'Michele',
+              avatar: '_1',
+            },
+            {
+              name: 'Fabio',
+              avatar: '_2',
+            },
+            {
+              name: 'Samuele',
+              avatar: '_3',
+            },
+            {
+              name: 'Luisa',
+              avatar: '_4',
+            },
+            ]
+        
+
+    }, methods: {
+    }
+})
