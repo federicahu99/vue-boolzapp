4bcd81e23050a02706ed4e460dbc235f203185a6
feddericahu99 <federica.hu339@gmail.com>
2022-07-15 17:34:06 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ const root = new Vue ({
         {
           date: '20/03/2020 16:35:00',
           text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-          status: 'received'
+          status: 'sent'
         }
         ],
       },
